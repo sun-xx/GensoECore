7aa9be3d31f53be8e4c305fc9e9c07ec340f835c
dstro <supershc@126.com>
2022-08-10 21:02:06 +0800

    完善碰撞检测；增加动圆检测重载、矩形与圆碰撞检测.

--- GensoEcore/Object/CollisionDetect.cpp
+++ GensoEcore/Object/CollisionDetect.cpp
@@ -33,7 +33,33 @@ inline bool Genso::CircleHit(sf::Vector2f pos1,float r1,
 inline bool Genso::CircleHit(sf::Vector2f pos1,sf::Vector2f last_pos1,float r1,
     sf::Vector2f pos2,sf::Vector2f last_pos2,float r2)
 {
-    
+    float X1 = pos1.x - pos2.x,
+    X2 = last_pos1.x - last_pos2.x - X1,
+    Y1 = pos1.y - pos2.y,
+    Y2 = last_pos1.y - last_pos2.y - Y1;
+
+    float a = X2*X2 + Y2*Y2,
+    b = 2*(X1*X2 + Y1*Y2),
+    c = X1*X1 + Y1*Y1 - r1*r1 - r2*r2,
+    delta = b*b - 4*a*c;
+
+    if(delta < 0) return false;
+
+    float t1 = (Genso::qc::Qsqrt(delta)-b)/(2*a),
+    t2 = (0-Genso::qc::Qsqrt(delta)-b)/(2*a);
+
+    return (t1>0 && t1<1) || (t2>0 && t2<1);
+}
+
+inline bool Genso::AABBCircleHit(sf::Vector2f pos1,sf::Vector2f size1,
+        sf::Vector2f pos2,float r)
+{
+    pos1.x -= r;
+    pos1.y += r;
+    size1.x += 2*r;
+    size1.y += 2*r;
+
+    return Genso::AABBHit(pos1,size1,pos2,0.f);
 }
 
 inline bool Genso::SegmentLaserHit(sf::Vector2f pos1,sf::Vector2f pos2,
--- GensoEcore/Object/CollisionDetect.hpp
+++ GensoEcore/Object/CollisionDetect.hpp
@@ -8,13 +8,16 @@ namespace Genso
     inline bool AABBHit(sf::Vector2f pos1,sf::Vector2f size1,
         sf::Vector2f pos2,sf::Vector2f size2);
 
-    inline bool Genso::CircleHit(sf::Vector2f pos1,float r1,
+    inline bool CircleHit(sf::Vector2f pos1,float r1,
         sf::Vector2f pos2,float r2);
 
-    inline bool Genso::CircleHit(sf::Vector2f pos1,sf::Vector2f last_pos1,float r1,
+    inline bool CircleHit(sf::Vector2f pos1,sf::Vector2f last_pos1,float r1,
         sf::Vector2f pos2,sf::Vector2f last_pos2,float r2);
 
-    inline bool Genso::SegmentLaserHit(sf::Vector2f pos1,sf::Vector2f pos2,
+    inline bool AABBCircleHit(sf::Vector2f pos1,sf::Vector2f size1,
+        sf::Vector2f pos2,float r);
+
+    inline bool SegmentLaserHit(sf::Vector2f pos1,sf::Vector2f pos2,
         sf::Vector2f myPos,float judgeValue);
 }
 
